1f50cde41bfa583bd6c3d7654c47562534df1ac1
GameCrucible <cpdepalm@gmail.com>
2023-02-10 22:47:56 -0600

    Projet

---
+++ DePalma_ApplePicker/Assets/Basket.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+    public ScoreCounter scoreCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreCounter = scoreGO.GetComponent<ScoreCounter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos2d = Input.mousePosition;
+
+
+        mousePos2d.z = -Camera.main.transform.position.z;
+
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2d);
+
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.CompareTag("Apple"))
+        {
+            Destroy(collidedWith);
+            scoreCounter.score += 100;
+            HighScore.TRY_SET_HIGH_SCORE(scoreCounter.score);
+        }
+    }
+}
